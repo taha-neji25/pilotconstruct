a629b9d3546ca460e89f18b4cda503c2f77837a6
taha-neji25 <tahaneji@protonmail.com>
2025-01-23 19:04:23 +0100

    added 404.html

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect } from 'react';
-import { useNavigate, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // No need for HashRouter here
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import About from "../pages/About";
@@ -9,37 +9,14 @@ import ProductDetail from "../components/Products/ProductDetail";
 import NotFound from "../pages/NotFound";
 
 function AppRoutes() {
-
-  const navigate = useNavigate();
-
-  function extractFullPath(url) {
-    // Remove the domain and any leading/trailing slashes
-    // eslint-disable-next-line no-useless-escape
-    const path = url.replace(/^https?:\/\/[^\/]+/, '').replace(/^\/|\/$/g, '');
-    return path;
-  }
-
-  useEffect(() => {
-    const currentPath = extractFullPath(window.location.href);
-    
-    // Define valid routes
-    const validRoutes = ['', 'about', 'products', 'contact', 'products/dao', 'products/menuiserie-metallique', 'products/construction-modulaire', 'products/structure-metallique', 'products/maintenance', 'products/construction-fabrication-serie', 'products/traitement-de-peinture']; // Add all valid routes here
-    // Check if the current path is valid
-    if (!validRoutes.includes(currentPath)) {
-      // Redirect to a 404 page or home page
-        navigate(`/`); // Or navigate('/') to redirect to home
-    }
-  }, [navigate]);
-
   return (
-    <Routes>
+    <Routes basename={"/"}>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
-      {/* Dynamic route for individual product pages */}
       <Route path="/products/:productName" element={<ProductDetail />} />
-      <Route path="*" element={<NotFound />} /> 
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
